66410bec3d029a74ae1d270ac811ec3c674726c9
Emre Bil <107650039+bilemre@users.noreply.github.com>
2022-09-19 15:50:03 +0300

    JAVA_CAMP_2022

---
+++ homework/week_1/variableNeedForUse/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ogrenciSayisi = 10;
+        String mesaj = "Öğrenci Sayım : ";
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println("Öğrenci Sayım : "+ogrenciSayisi);
+        System.out.println("Öğrenci Sayım : "+ogrenciSayisi);
+        System.out.println("Öğrenci Sayım : "+ogrenciSayisi);
+        System.out.println("Öğrenci Sayım : "+ogrenciSayisi);
+
+    }
+}
