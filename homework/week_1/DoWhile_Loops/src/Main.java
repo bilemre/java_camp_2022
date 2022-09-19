337285b4cebead5a8e197792c75d59b0f85badf2
Emre Bil <107650039+bilemre@users.noreply.github.com>
2022-09-19 16:06:05 +0300

    JAVA_CAMP_2022

---
+++ homework/week_1/DoWhile_Loops/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int j = 2;
+        do {
+            System.out.println(j);
+            j+=2;
+        }while(j<=10);
+        System.out.println("Loop over!");
+    }
+}
