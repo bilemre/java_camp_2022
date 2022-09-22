39a0a2b0c171ac081d8e416d6a24b4e9de22f688
Emre Bil <107650039+bilemre@users.noreply.github.com>
2022-09-22 19:54:55 +0300

    Update Main.java

--- homework/week_2/ValueAndReferenceTypes/src/Main.java
+++ homework/week_2/ValueAndReferenceTypes/src/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
         System.out.println(numbers2[0]);
 
-
+        //System.out.println(number2);     --> output  : 10
+        //System.out.println(numbers2[0]); --> output  : 10
     }
 }
