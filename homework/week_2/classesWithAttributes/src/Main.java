a370835d4258477683a2d3f6f9f8b55552a1d4bc
Emre Bil <107650039+bilemre@users.noreply.github.com>
2022-09-22 23:41:52 +0300

    classesWithAttributes

---
+++ homework/week_2/classesWithAttributes/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Product product = new Product();
+
+        product.name="Laptop";
+        product.id=1;
+        product.description="Asus Laptop";
+        product.price=5000;
+        product.stockAmount=3;
+
+    ProductManager productManager = new ProductManager();
+
+        productManager.add(product);
+
+
+
+    }
+
+
+}
---
+++ homework/week_2/classesWithAttributes/src/Product.java
@@ -0,0 +1,8 @@
+public class Product {
+    String name;
+    int id;
+    String description;
+    int price;
+    int stockAmount;
+
+}
