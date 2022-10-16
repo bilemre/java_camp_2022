a1960894391a91e3e76f6f2ab29e5d607cebbcc4
Emre Bil <107650039+bilemre@users.noreply.github.com>
2022-10-16 21:21:00 +0300

    interfacesDemo

---
+++ homework/week_3/interfacesDemo/src/Worker.java
@@ -0,0 +1,16 @@
+public class Worker implements IWorkable,IEatable,IPayable{
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void pay() {
+
+    }
+
+    @Override
+    public void work() {
+
+    }
+}
